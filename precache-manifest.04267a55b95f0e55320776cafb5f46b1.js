23acfa9fb8ddf7f329096d4910f685f949e03125
pleomax13 <43319882+pleomax13@users.noreply.github.com>
2019-04-18 00:36:14 +0300

    Updates

---
+++ precache-manifest.04267a55b95f0e55320776cafb5f46b1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "743eec75a708ce2d2985",
+    "url": "/folio/static/css/main.6daf186b.chunk.css"
+  },
+  {
+    "revision": "743eec75a708ce2d2985",
+    "url": "/folio/static/js/main.743eec75.chunk.js"
+  },
+  {
+    "revision": "a5f0a3f3925517526f1c",
+    "url": "/folio/static/js/1.a5f0a3f3.chunk.js"
+  },
+  {
+    "revision": "f792f8e0669550df4387",
+    "url": "/folio/static/js/runtime~main.f792f8e0.js"
+  },
+  {
+    "revision": "cc4c5620bc2044c5dd5d351afd8027df",
+    "url": "/folio/static/media/lot(9).cc4c5620.JPG"
+  },
+  {
+    "revision": "335854a37abf5569ba0166e8bdea856f",
+    "url": "/folio/static/media/lot(10).335854a3.JPG"
+  },
+  {
+    "revision": "1df37bc2d888a6aed4e3b12e9727dbaa",
+    "url": "/folio/static/media/bin.1df37bc2.svg"
+  },
+  {
+    "revision": "4499b027d495a6d215dae70469fcdd1a",
+    "url": "/folio/static/media/pencil.4499b027.svg"
+  },
+  {
+    "revision": "508adc52f97d970279c625c31a08ee43",
+    "url": "/folio/static/media/BebasNeueRegular.508adc52.woff"
+  },
+  {
+    "revision": "99a562fc4aa76505c043308d57ff7aa7",
+    "url": "/folio/static/media/OpenSansLight-Italic.99a562fc.woff"
+  },
+  {
+    "revision": "c426bf8b99deff32e78e9261ed835e74",
+    "url": "/folio/static/media/OpenSans.c426bf8b.woff"
+  },
+  {
+    "revision": "6cfaf3d609a6fdcf0e4a9cf004481db2",
+    "url": "/folio/static/media/OpenSans-Semibold.6cfaf3d6.woff"
+  },
+  {
+    "revision": "16321bf350af4899068ec64fd3f753e4",
+    "url": "/folio/static/media/OpenSans-SemiboldItalic.16321bf3.woff"
+  },
+  {
+    "revision": "323774fa58dfbb9c00e1a948371c9eed",
+    "url": "/folio/static/media/BebasNeueRegular.323774fa.eot"
+  },
+  {
+    "revision": "7ad3200721e27507ddb6e8380fb5dfaa",
+    "url": "/folio/static/media/OpenSansLight-Italic.7ad32007.eot"
+  },
+  {
+    "revision": "5cca0e3c54ea6d2c2968bdcacb1fa8d7",
+    "url": "/folio/static/media/OpenSans.5cca0e3c.eot"
+  },
+  {
+    "revision": "9042a37caa7fb0389371de54dd886b38",
+    "url": "/folio/static/media/BebasNeueRegular.9042a37c.ttf"
+  },
+  {
+    "revision": "73f7301a9cd7a086295401eefe0c998f",
+    "url": "/folio/static/media/OpenSans-SemiboldItalic.73f7301a.ttf"
+  },
+  {
+    "revision": "6943fb6fd4200f3d073469325c6acdc9",
+    "url": "/folio/static/media/OpenSans-LightItalic.6943fb6f.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/folio/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/folio/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "5396b42d3b512081e11742d1b298a256",
+    "url": "/folio/static/media/photo_head.5396b42d.jpg"
+  },
+  {
+    "revision": "984f01e9d1e372319a92a7c7a11e339c",
+    "url": "/folio/static/media/photo_head-2x.984f01e9.jpg"
+  },
+  {
+    "revision": "474d6b057bfd608ac2b3bbb09efa960e",
+    "url": "/folio/static/media/footer.474d6b05.jpg"
+  },
+  {
+    "revision": "844129ffea9f26264e5fe5100c4b1309",
+    "url": "/folio/static/media/footer-2x.844129ff.jpg"
+  },
+  {
+    "revision": "abbc70ca91b8fe34a61575b967e22e4a",
+    "url": "/folio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/folio/precache-manifest.04267a55b95f0e55320776cafb5f46b1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/folio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
